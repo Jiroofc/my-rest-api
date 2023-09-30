54194df40b6ff9f392404dcbed42cff2113f77b8
Jiroofc <145113188+Jiroofc@users.noreply.github.com>
2023-09-30 12:55:07 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Jiro'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://Jiroofc:S7iikxm6QKTDuFSS@cluster0.jia1yhc.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "Jiroprem" //isi apa aja bebas
 global.your_email = "ownerzrijo@gmail.com" //email
 global.email_password = "jirokw001" //application password email
