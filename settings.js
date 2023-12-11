1d4abdbe7c6659d1f5622ccdfe69822019c5beec
Jiroofc <145113188+Jiroofc@users.noreply.github.com>
2023-12-11 20:26:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Jiro'// yourname
 global.MONGO_DB_URI = "mongodb+srv://Jiroofc:S7iikxm6QKTDuFSS@cluster0.jia1yhc.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "Jiroprem" //isi apa aja bebas
-global.your_email = "ownerzrijo@gmail.com" //email
-global.email_password = "jirokw001" //application password email
+global.your_email = "gamewibu4@gmail.com" //email
+global.email_password = "YVDQ 8uUt susV" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
